ec9d0a262dc59845b0231dda375e5cd115e1dcb2
thusitha <thusitha.4t@gmail.com>
2019-08-16 08:28:01 +0530

    Initial Commit

---
+++ posty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  posty
+//
+//  Created by thusitha on 8/16/19.
+//  Copyright © 2019 noname. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
